bf4ed94c4385b9f5ba6908ed36a1d28725f74584
wilzzini <wilson.scavazzini@beblue.com.br>
2019-06-21 12:49:11 -0300

    add tests

--- app.py
+++ app.py
@@ -36,4 +36,4 @@ api.add_resource(CityList, '/cities')
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
